8a678615eb57a88550b7c87b10aed90ed04a350b
Juan Pablo Martínez <jpablo.mguzman@gmail.com>
2025-02-04 12:10:52 -0500

    feat: create puzzle camera pov

--- Assets/Scripts/Controllers/CamerasController.cs
+++ Assets/Scripts/Controllers/CamerasController.cs
@@ -7,12 +7,15 @@ public class CamerasController : MonoBehaviour
     [Header("Cameras")]
     [SerializeField] private GameObject initialWorldCamera;
     [SerializeField] private GameObject puzzleCamera;
+    [SerializeField] private GameObject freeLookCamera;
+
 
     [Header("Animation Values")]
     [SerializeField] private float animationTime;
 
     private void Start() {
         StartCoroutine(TurnCamera(initialWorldCamera, animationTime, false));
+        StartCoroutine(TurnCamera(freeLookCamera, animationTime-1, true));
     }
 
 
@@ -21,6 +24,21 @@ public class CamerasController : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
         obj.SetActive(value);
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(other.CompareTag("Player"))
+        {
+            puzzleCamera.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            puzzleCamera.SetActive(false);
+        }
     }
 
 
